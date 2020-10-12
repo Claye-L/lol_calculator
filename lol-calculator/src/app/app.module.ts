eaa166b1f0ee0f9afa9f238e886f655cab046285
Claye_L <claye@ixcys.fr>
2020-10-12 20:30:11 +0200

    yolo first steps

--- lol-calculator/src/app/app.module.ts
+++ lol-calculator/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChampionListComponent } from './champion-list/champion-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChampionListComponent
   ],
   imports: [
     BrowserModule,
