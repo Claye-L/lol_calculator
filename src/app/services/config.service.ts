d1c02a19d36bcf438cf5a7161b0b16688cb77dca
Claye_L <claye@ixcys.fr>
2020-10-20 15:32:27 +0200

    add ts api models pkg

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Champion} from "../models/Champion"
+
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class ConfigService {
-  constructor(private http: HttpClient) { }
-    championsUrl = 'assets/champion.json';
-    getChampions() {
-        return this.http.get<Champion>(this.championsUrl);
-    }
+  
 }
